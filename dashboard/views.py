2e5124a02a0d75b60fbdb90a1b617c246020b720
samantha moss <samantha.moss87@aol.com>
2025-05-29 19:51:03 +0000

    Added user's past and future bookings in the dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
+from booking.models import Booking
+from booking.forms import BookingForm
 
 @login_required
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+
+    today = datetime.today().date()
+
+    future_bookings = Booking.objects.filter(user=request.user, date__gte=today).order_by('date', 'start_time')
+    past_bookings = Booking.objects.filter(user=request.user, date__lt=today).order_by('-date', '-start_time')
+
+    context = {
+        'future_bookings': future_bookings,
+        'past_bookings': past_bookings,
+    }
+
+    return render(request, 'dashboard/dashboard.html', context)
