29339ac2fc880ed29f019b4034c5d16a54d4d8b8
samanthamoss87 <samantha.moss87@aol.com>
2025-11-01 17:54:20 +0000

    Added the Model to the admin so that admin can see subscriber's emails

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Subscribe
 
-# Register your models here.
+@admin.register(Subscribe)
+class SubscribeAdmin(admin.ModelAdmin):
+    list_display = ('email', 'is_active')
+    list_filter = ('is_active',)
+    search_fields = ('email',)
