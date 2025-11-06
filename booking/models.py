aab5ca50d4467fd6e16ae469f9bfa9eca71fb0b4
samanthamoss87 <samantha.moss87@aol.com>
2025-11-06 17:25:34 +0000

    Added cancel booking functionality and validate negative value from form

--- booking/models.py
+++ booking/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.conf import settings
+from django.core.validators import MinValueValidator
 
 
 class Treatments(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    half_hour = models.DecimalField(max_digits=6, decimal_places=2, default=55.00)
-    one_hour = models.DecimalField(max_digits=6, decimal_places=2, default=80.00)
-    two_hour = models.DecimalField(max_digits=6, decimal_places=2, default=110.00)
+    half_hour = models.DecimalField(max_digits=6, decimal_places=2, default=55.00, validators=[MinValueValidator(1)])
+    one_hour = models.DecimalField(max_digits=6, decimal_places=2, default=80.00, validators=[MinValueValidator(1)])
+    two_hour = models.DecimalField(max_digits=6, decimal_places=2, default=110.00, validators=[MinValueValidator(1)])
     
 
     def __str__(self):
@@ -16,11 +17,17 @@ class Treatments(models.Model):
 
 
 class Booking(models.Model):
+    BOOKING_STATUS = [
+        ('confirmed', 'Confirmed'),
+        ('cancelled', 'Cancelled'),
+    ]
+    
     treatment = models.ForeignKey(Treatments, on_delete=models.CASCADE)
     date = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()
     duration = models.IntegerField()
+    status = models.CharField(max_length=20, choices=BOOKING_STATUS, default='confirmed')
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
@@ -29,6 +36,13 @@ class Booking(models.Model):
         start = datetime.combine(self.date, self.start_time)
         self.end_time = (start + timedelta(minutes=self.duration)).time()
         super().save(*args, **kwargs)
+
+    def can_cancel(self):
+        from datetime import datetime, timedelta
+        booking_datetime = datetime.combine(self.date, self.start_time)
+        current_datetime = datetime.now()
+        time_difference = booking_datetime - current_datetime
+        return time_difference.total_seconds() >= 24 * 3600
 
     def __str__(self):
         return f"{self.user } - {self.treatment.title} on {self.date} at {self.start_time} for {self.duration} minutes"
